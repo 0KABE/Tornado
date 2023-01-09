5ca02c929f21c6f66a1f0e833d963862b4f7b0fc
0KABE <RIN.OKAB3@GMAIL.COM>
2023-01-10 02:45:44 +0800

    [CHORE]: Add some comments

--- src/base.cc
+++ src/base.cc
@@ -2,4 +2,6 @@
 // Created by Chen, WenTao on 2023/1/10.
 //
 
+// This file does not contain any source code
+// The purpose of this file is to satisfy that CMake add_library() should have one source file at least
 #include "base.h"
