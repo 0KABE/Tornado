ee101076e0982976efbcc7a966bcd86457846d4e
0KABE <RIN.OKAB3@GMAIL.COM>
2023-01-22 00:18:43 +0800

    [CHORE]: Remove CLI11 dependency
    GTest build will be failed with CLI11 in ASAN mode if CLI11 is enabled

--- test/main.cc
+++ test/main.cc
@@ -4,43 +4,13 @@
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
 
-#include <CLI/CLI.hpp>
-#include <set>
-#include <vector>
-
-struct CLIOptions {
-  std::string log_level;
-};
-
-void InitCLI(CLI::App &app, CLIOptions &cli_options) {
-  std::set<std::string> levels;
-  {
-    std::set<spdlog::string_view_t> spdlog_levels(SPDLOG_LEVEL_NAMES);
-    std::transform(
-        spdlog_levels.begin(), spdlog_levels.end(), std::inserter(levels, levels.begin()),
-        [](spdlog::string_view_t level) { return std::string(level.data(), level.size()); });
-  }
-
-  app.add_option(
-         "-l,--log", cli_options.log_level,
-         fmt::format("Set log level in [{}]",
-                     fmt::join(std::vector<spdlog::string_view_t>(SPDLOG_LEVEL_NAMES), ", ")))
-      ->type_name("LEVEL")
-      ->check(CLI::IsMember(levels))
-      ->default_val("info");
-}
-
-int main(int argc, char **argv) {
-  CLI::App app;
-  CLIOptions cli_options;
-
+int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
 
-  InitCLI(app, cli_options);
-  CLI11_PARSE(app, argc, argv)
-
-  spdlog::set_level(spdlog::level::from_str(cli_options.log_level));
-  spdlog::set_pattern("[%T.%e] [%L] [%t] %v");
+  if (const char* log_level = getenv("LOG_LEVEL")) {
+    spdlog::set_level(spdlog::level::from_str(log_level));
+    spdlog::set_pattern("[%T.%e] [%L] [%t] %v");
+  }
 
   return RUN_ALL_TESTS();
 }
