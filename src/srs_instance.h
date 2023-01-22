ffaf7dd8867627201362f3c51ce5a2c81145a408
0KABE <RIN.OKAB3@GMAIL.COM>
2023-01-22 18:59:39 +0800

    [DOC]: Add some comments

--- src/srs_instance.h
+++ src/srs_instance.h
@@ -13,7 +13,17 @@ class SRSInstance {
   SRSInstance(int argc, char** argv);
 
   void Run();
+
+  /**
+   * Like as gracefully quit in SRS, thread will not stop until there are no connection
+   */
   void GracefullyStop();
+
+  /**
+   * Like as fast quit in SRS, thread will stop quickly.
+   *
+   * @note The behavior can be modified to force gracefully quit in config
+   */
   void FastStop();
 
   ~SRSInstance() = default;
