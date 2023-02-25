3f3ec1fb37acef48f0ce9672bd3f8d1f503da5a8
0KABE <RIN.OKAB3@GMAIL.COM>
2023-02-26 02:51:11 +0800

    [FEAT]: Update life cycle management for RTMPStream

--- include/tornado/rtmp_stream.h
+++ include/tornado/rtmp_stream.h
@@ -8,20 +8,27 @@
 #include <srs_app_source.hpp>
 
 namespace Tornado {
-class RTMPStream : public ISrsLiveSourceBridger {
+class RTMPStream {
  public:
   using SourceDeleter = void (*)(SrsLiveSource*);
 
   explicit RTMPStream(SrsLiveSource* srs_live_source);
 
  private:
-  srs_error_t on_publish() override;
-  srs_error_t on_audio(SrsSharedPtrMessage* audio) override;
-  srs_error_t on_video(SrsSharedPtrMessage* video) override;
-  void on_unpublish() override;
-
   static void ReleaseSource(SrsLiveSource* srs_live_source);
 
+  struct SrsLiveSourceBridger : public ISrsLiveSourceBridger {
+    explicit SrsLiveSourceBridger(RTMPStream* rtmp_stream);
+
+    srs_error_t on_publish() override;
+    srs_error_t on_audio(SrsSharedPtrMessage* audio) override;
+    srs_error_t on_video(SrsSharedPtrMessage* video) override;
+    void on_unpublish() override;
+
+    RTMPStream* rtmp_stream_;
+  };
+
   std::unique_ptr<SrsLiveSource, SourceDeleter> srs_live_source_;
+  SrsLiveSourceBridger* live_source_bridger_;
 };
 }  // namespace Tornado
--- src/rtmp_stream.cc
+++ src/rtmp_stream.cc
@@ -7,9 +7,10 @@
 #include <spdlog/spdlog.h>
 
 Tornado::RTMPStream::RTMPStream(SrsLiveSource *srs_live_source)
-    : srs_live_source_(srs_live_source, &ReleaseSource) {
+    : srs_live_source_(srs_live_source, &ReleaseSource),
+      live_source_bridger_(new SrsLiveSourceBridger(this)) {
   spdlog::debug("srs_live_source_: {}, RTMPStream: {}", fmt::ptr(srs_live_source), fmt::ptr(this));
-  srs_live_source_->set_bridger(this);
+  srs_live_source_->set_bridger(live_source_bridger_);
 }
 
 // TODO: ReleaseSource should make sure that be executed in SRS thread, or there is condition racing
@@ -18,16 +19,19 @@ void Tornado::RTMPStream::ReleaseSource(SrsLiveSource *srs_live_source) {
   srs_live_source->on_unpublish();
 }
 
-srs_error_t Tornado::RTMPStream::on_publish() { return nullptr; }
+srs_error_t Tornado::RTMPStream::SrsLiveSourceBridger::on_publish() { return nullptr; }
 
-srs_error_t Tornado::RTMPStream::on_audio(SrsSharedPtrMessage *audio) {
+srs_error_t Tornado::RTMPStream::SrsLiveSourceBridger::on_audio(SrsSharedPtrMessage *audio) {
   spdlog::debug("received audio frame from SRS");
   return srs_success;
 }
 
-srs_error_t Tornado::RTMPStream::on_video(SrsSharedPtrMessage *video) {
+srs_error_t Tornado::RTMPStream::SrsLiveSourceBridger::on_video(SrsSharedPtrMessage *video) {
   spdlog::debug("received video frame from SRS");
   return srs_success;
 }
 
-void Tornado::RTMPStream::on_unpublish() {}
+void Tornado::RTMPStream::SrsLiveSourceBridger::on_unpublish() {}
+
+Tornado::RTMPStream::SrsLiveSourceBridger::SrsLiveSourceBridger(Tornado::RTMPStream *rtmp_stream)
+    : rtmp_stream_(rtmp_stream) {}
